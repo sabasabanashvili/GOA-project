f4a850af24222787c2a83fd4adb93b189305ff17
sabasabanashvili <sabanashv@gmail.com>
2024-10-10 10:58:15 -0700

    Add files via upload
    s

---
+++ day 3/s.py
@@ -0,0 +1,15 @@
+name = "saba"
+fullname = "sabanashvili"
+age = 14
+study = "goa"
+good = "goa"
+print(name)
+print(fullname)
+print(age)
+print("saba")
+print("sabanashili")
+print("mc")
+print("goastudent")
+print("imman")
+print("saba sabanashvili")
+print("imSmallChadBcImStudyInGoa")
