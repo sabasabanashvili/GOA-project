93c7be46c992b4b074d83a67d762437024bd7c33
sabasabanashvili <sabanashv@gmail.com>
2024-11-08 10:18:12 -0800

    commit:

---
+++ day 10/homework/homework1.py
@@ -0,0 +1,247 @@
+#მეტობა
+a = 10
+s = 5
+print(a > s)
+
+d = 3
+f = 7
+print(d > f)
+
+g = 20
+h = 10
+print(g > h)
+
+j = 50
+k = 30
+print(j > k)
+
+l = 15
+p = 10
+print(l > p)
+
+o = 30
+i = 25
+print(o > i)
+
+u = 8
+y = 5
+print(u > y)
+
+t = 100
+r = 50
+print(t > r)
+
+e = 12
+w = 10
+print(e > w)
+
+q = 40
+z = 35
+print(q > z)
+
+#ნაკლებობა
+x = 5
+c = 10
+print(x < c)
+
+v = 12
+b = 15
+print(v < b)
+
+n = 20
+m = 25
+print(n < m)
+
+a = 18
+s = 20
+print(a < s)
+
+d = 10
+f = 9
+print(d < f)
+
+g = 30
+h = 35
+print(g < h)
+
+j = 50
+k = 100
+print(j < k)
+
+l = 45
+p = 60
+print(l < p)
+
+o = 75
+i = 100
+print(o < i)
+
+u = 60
+y = 80
+print(u < y)
+
+#მეტობა ან ტოლობა
+t = 20
+r = 20
+print(t >= r)
+
+e = 25
+w = 20
+print(e >= w)
+
+q = 50
+z = 50
+print(q >= z)
+
+x = 15
+c = 10
+print(x >= c)
+
+v = 30
+b = 25
+print(v >= b)
+
+n = 40
+m = 30
+print(n >= m)
+
+a = 10
+s = 5
+print(a >= s)
+
+d = 12
+f = 10
+print(d >= f)
+
+g = 100
+h = 75
+print(g >= h)
+
+j = 60
+k = 60
+print(j >= k)
+
+#ნაკლებობა ანდ ტოლობა
+a = 5
+s = 10
+print(a <= s)
+
+d = 15
+f = 20
+print(d <= f)
+
+g = 30
+h = 30
+print(g <= h)
+
+j = 25
+k = 40
+print(j <= k)
+
+l = 10
+p = 15
+print(l <= p)
+
+o = 50
+i = 55
+print(o <= i)
+
+u = 60
+y = 70
+print(u <= y)
+
+t = 10
+r = 10
+print(t <= r)
+
+e = 80
+w = 90
+print(e <= w)
+
+q = 100
+z = 150
+print(q <= z)
+
+#არ არის ტოლი
+x = 5
+c = 10
+print(x != c)
+
+v = 12
+b = 12
+print(v != b)
+
+n = 20
+m = 30
+print(n != m)
+
+a = 15
+s = 20
+print(a != s)
+
+d = 50
+f = 50
+print(d != f)
+
+g = 10
+h = 20
+print(g != h)
+
+j = 40
+k = 30
+print(j != k)
+
+l = 60
+p = 60
+print(l != p)
+
+o = 75
+i = 80
+print(o != i)
+
+u = 90
+y = 100
+print(u != y)
+
+#ტოლია
+a = 10
+s = 10
+print(a == s)
+
+d = 20
+f = 20
+print(d == f)
+
+g = 15
+h = 25
+print(g == h)
+
+j = 50
+k = 50
+print(j == k)
+
+l = 100
+p = 100
+print(l == p)
+
+o = 5
+i = 5
+print(o == i)
+
+u = 30
+y = 30
+print(u == y)
+
+t = 60
+r = 60
+print(t == r)
+
+e = 200
+w = 200
+print(e == w)
+
+q = 25
+z = 25
+print(q == z)
+
+
