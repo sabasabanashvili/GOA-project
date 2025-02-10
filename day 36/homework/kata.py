163594758a173f43302f24f2ff444731f5d72ef5
sabasabanashvili <sabanashv@gmail.com>
2025-02-10 10:52:30 -0800

    commit:

---
+++ day 36/homework/kata.py
@@ -0,0 +1,14 @@
+#1
+def find_smallest_integer(arr):
+    return min(arr)
+
+print(find_smallest_integer([34, 15, 88, 2]))  
+print(find_smallest_integer([34, -345, -1, 100]))
+
+#2
+def string_to_number(s):
+    return int(s)
+
+#3
+def summation(num):
+    return num * (num + 1) // 2
